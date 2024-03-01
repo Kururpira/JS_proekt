6d18fd338430c21c8bbd13938405f72e34c70c19
Kururpira <kururpira@inbox.ru>
2024-03-01 13:01:14 +0300

    commit

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const ingridients = document.getElementById('ingridients');
+const ingridients = document.getElementById('ingridients').value;
 const button = document.getElementById('button');
 const qty = document.getElementById('qty');
 const unit = document.getElementById('unit');
 const food = document.getElementById('food');
-const calories = document.getElementById('calories');
+let calories = document.getElementById('calories');
 const weight = document.getElementById('weight');
 
-
-fetch(''
-, {
-  method: 'GET',
-  mode: 'no-cors'
-})
-  .then(response =>response.json())
-  .then(data => console.log(data))
-  .catch(error =>console.log('Error:', error));
-
+/*function onBtnClick(){
+fetch('https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20apple')
+  .then(response => response.json())
+  .then(data => calories.textContent = data.calories,
+    weight.textContent = data.totalWeight)
+}*/
+function onBtnClick(){
+const API = `https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20${ingridients}`
+const fetchResult = fetch(API).then((res) => res.json()).then((data) => {calories.textContent = data.calories});
+}
 /*
   .then(response => response.json())
   .then(data => console.log(data.ingr))
-  .catch(error => console.log(error))*/
+  .catch(error => console.log(error))
+  information?apiKey=618b54d6b79d47e8a682a33d950e7d44*/
