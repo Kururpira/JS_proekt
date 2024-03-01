db602031a2c75c4ceeb092ac649a4263a5c6d368
Kururpira <kururpira@inbox.ru>
2024-03-01 17:10:32 +0300

    commit

--- script.js
+++ script.js
@@ -14,15 +14,17 @@ const sugarResult = fetch(API).then((res) => res.json()).then((data) => {sugar.t
 const ironResult = fetch(API).then((res) => res.json()).then((data) => {iron.textContent = Math.ceil(data.totalNutrients.FE.quantity)});
 const vitaminCResult = fetch(API).then((res) => res.json()).then((data) => {vitaminC.textContent = Math.ceil(data.totalNutrients.VITC.quantity)});
 const calciumCResult = fetch(API).then((res) => res.json()).then((data) => {calcium.textContent = Math.ceil(data.totalNutrients.CA.quantity)});
+
 }
 
 
 function onBtnClickCleaner(){
-  const API = `https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20${ingridients}`
-  const caloriesResult = fetch(API).then((res) => res.json()).then((data) => {calories.textContent =0});
+  
+  analysisForm.reset();
+};
+/* const API = `https://api.edamam.com/api/nutrition-data?app_id=d7be0f59&app_key=7670b7efd74aa8278e4343bfd8644a49&nutrition-type=cooking&ingr=1%20${ingridients}`
+  const caloriesResult = fetch(API).then((res) => res.json()).then((data) => {calories.textContent = 0});
 const sugarResult = fetch(API).then((res) => res.json()).then((data) => {sugar.textContent = 0});
 const ironResult = fetch(API).then((res) => res.json()).then((data) => {iron.textContent = 0});
 const vitaminCResult = fetch(API).then((res) => res.json()).then((data) => {vitaminC.textContent = 0});
-const calciumCResult = fetch(API).then((res) => res.json()).then((data) => {calcium.textContent = 0});
-  analysisForm.reset();
-}
+const calciumCResult = fetch(API).then((res) => res.json()).then((data) => {calcium.textContent = 0}); */
